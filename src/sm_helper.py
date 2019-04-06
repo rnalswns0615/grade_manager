edaefb02c6ab3b97d3445565f222e30cc1cfa13b
rnalswns0615 <48683282+rnalswns0615@users.noreply.github.com>
2019-04-06 16:49:45 +0900

    190406 학생 난수 입력
    난수로 학생을 만들어서 입력

--- src/sm_helper.py
+++ src/sm_helper.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 19 20:51:34 2019
-확인하기위한 패스
+
 @author: 구상수
 """
 import operator
@@ -11,7 +11,7 @@ from random import *
 class StudentManager:
     def __init__(self):
         pass
-
+    
     @staticmethod
     def get_ranking(st_name, grade_dic):
         sorted_dic = sorted(grade_dic.items(), key=operator.itemgetter(1),reverse=True)
@@ -21,17 +21,16 @@ class StudentManager:
                 count = i + 1
                 break
         return count
-
-
-
+    
+    
     def view_grade_old(self, name):
         f =  open("C:/Users/구상수/.spyder-py3/study/grade.txt", 'r')
         lines = f.readlines()
         for line in lines:
-
+        
             if line.find(name) >= 0:
                 print(line.split(':')[1])
-    @staticmethod
+    @staticmethod        
     def view_grade(name):
         avg = {}
         math = {}
@@ -39,7 +38,7 @@ class StudentManager:
         korean = {}
         physics = {}
         alchemy = {}
-
+        
         f =  open("C:/Users/구상수/.spyder-py3/study/grade.txt", 'r')
         lines = f.readlines()
         total = len(lines)
@@ -57,30 +56,43 @@ class StudentManager:
             physics[student] = int(all_grade[3])
             alchemy[student] = int(all_grade[4])
         grade_report = {}
-        grade_report['avg'] = [avg[name],
+        grade_report['avg'] = [avg[name], 
                              StudentManager.get_ranking(name, avg),
                              round(StudentManager.get_ranking(name, avg) / total * 100, 2)]
-        grade_report['math'] = [math[name],
+        grade_report['math'] = [math[name], 
                              StudentManager.get_ranking(name, math),
                              round(StudentManager.get_ranking(name, math) / total * 100, 2)]
-        grade_report['english'] = [english[name],
+        grade_report['english'] = [english[name], 
                              StudentManager.get_ranking(name, english),
                              round(StudentManager.get_ranking(name, english) / total * 100, 2)]
-        grade_report['korean'] = [korean[name],
+        grade_report['korean'] = [korean[name], 
                              StudentManager.get_ranking(name, korean),
                              round(StudentManager.get_ranking(name, korean) / total * 100, 2)]
-        grade_report['physics'] = [physics[name],
+        grade_report['physics'] = [physics[name], 
                              StudentManager.get_ranking(name, physics),
                              round(StudentManager.get_ranking(name, physics) / total * 100, 2)]
-        grade_report['alchemy'] = [alchemy[name],
+        grade_report['alchemy'] = [alchemy[name], 
                              StudentManager.get_ranking(name, alchemy),
                              round(StudentManager.get_ranking(name, alchemy) / total * 100, 2)]
         print(grade_report)
 
-
-
-
+        
+                
+    
     def write_grade(self, name, math, english, korean, physics, alchemy):
+        fr = open("C:/Users/구상수/.spyder-py3/study/grade.txt", 'r')
+        lines = fr.readlines()
+        
+        count = 0
+        
+        for line in lines:
+            if line.find(name) >= 0:
+                count = count + 1
+                
+        if count > 0:
+            name = name + str(count)
+            print(name)
+        
         f =  open("C:/Users/구상수/.spyder-py3/study/grade.txt", 'a')
         student = '{}:{}\t{}\t{}\t{}\t{}'.format(
                     name,
@@ -90,19 +102,16 @@ class StudentManager:
                     physics,
                     alchemy
                     )
-        print(student)
+#        print(student)
         f.write('\n')
         f.write(student)
         f.close()
-
+        
 #    @staticmethod
 #    def write_rd_grade(loop):
-#
-
+#        
+      
 
 if __name__ == '__main__':
     sm = StudentManager()
-    sm.view_grade("김민석")
-#    sm.write_grade("김성재", '99', '45', '4', '0', '0')
-    sm.view_grade("김승재")
-    sm.view_grade("김성재")
+
